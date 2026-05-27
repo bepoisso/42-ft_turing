8f852063ba7889e398942f5944851d98fa3a955c
bepoisso <github@bepoisso.com>
2026-05-27 15:43:22 +0200

    fix: tape type declaration

--- srcs/types.ml
+++ srcs/types.ml
@@ -6,13 +6,29 @@ type transition = {
 	read : char;
 	write : char;
 	action : action;
+	from_state : string;
 	to_state : string;
 }
 
-type machine = {
+type tape = {
+	left : char list;
+	current : char;
+	right : char list;
+}
+
+type configuration = {
   name : string;
   alphabet : string list;
   blank : string;
   states : string list;
   initial : string;
+	finals : string;
+}
+
+
+type machine = {
+	config			: configuration;
+	tape 				: tape;
+	state				: string;
+	transitions	: transition list;
 }

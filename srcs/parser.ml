d24a4e96f64f1b07fa020b14a20fb74a86e7ee20
Evan <evan.araujo29@gmail.com>
2026-05-29 10:19:38 +0200

    all the parsing from the file

--- srcs/parser.ml
+++ srcs/parser.ml
@@ -2,8 +2,8 @@ let check_args =
   if (List.mem "-h" (Array.to_list Sys.argv) || List.mem "--help" (Array.to_list Sys.argv)) then (
     print_endline "usage: ft_turing [-h] jsonfile input\n";
     print_endline "positional arguments:";
-    print_endline "jsonfile       json description of the machine\n";
-    print_endline "input          input of the machine\n";
+    print_endline "jsonfile       json description of the configuration\n";
+    print_endline "input          input of the configuration\n";
     print_endline "optional arguments:";
     print_endline "-h, --help     show this help message and exit";
     exit 0
@@ -29,7 +29,7 @@ let check_file filename =
       print_endline "Invalid JSON";
       exit 1
 
-let check_alphabet json = 
+let check_alphabet json =
   let lst = (json |> Yojson.Basic.Util.member "alphabet" |> Yojson.Basic.Util.to_list |> List.map Yojson.Basic.Util.to_string) in
   if not (List.for_all (fun x -> String.length x = 1) lst) then (
     print_endline "Each element of the alphabet must be a string of length 1";
@@ -57,26 +57,103 @@ let check_initial json =
   );
   initial
 
-let get_info json : Types.machine =
+let check_finals json =
+  let finals = (json |> Yojson.Basic.Util.member "finals" |> Yojson.Basic.Util.to_list |> List.map Yojson.Basic.Util.to_string) in
+  if not (List.for_all (fun x -> List.mem x (json |> Yojson.Basic.Util.member "states" |> Yojson.Basic.Util.to_list |> List.map Yojson.Basic.Util.to_string)) finals) then (
+    print_endline "Each element of finals field must be part of states field";
+    exit 1
+  );
+  finals
+
+let parse_action str =
+  match str with
+  | "LEFT"  -> Types.Left
+  | "RIGHT" -> Types.Right
+  | _       ->
+    Printf.printf "Invalid action: %s\n" str;
+    exit 1
+
+let parse_transition root_json trans_json : Types.transition =
+  let alphabet = root_json |> Yojson.Basic.Util.member "alphabet" |> Yojson.Basic.Util.to_list |> List.map Yojson.Basic.Util.to_string in
+  let states   = root_json |> Yojson.Basic.Util.member "states"   |> Yojson.Basic.Util.to_list |> List.map Yojson.Basic.Util.to_string in
+  let read     = trans_json |> Yojson.Basic.Util.member "read"     |> Yojson.Basic.Util.to_string in
+  if not (List.mem read alphabet) then (
+    print_endline "Read element must be part of the alphabet field";
+    exit 1
+  );
+  let read_char = read.[0] in
+  let to_state = trans_json |> Yojson.Basic.Util.member "to_state" |> Yojson.Basic.Util.to_string in
+  if not (List.mem to_state states) then (
+    print_endline "to_state must be part of states field";
+    exit 1
+  );
+  let write  = trans_json |> Yojson.Basic.Util.member "write"  |> Yojson.Basic.Util.to_string in
+  if not (List.mem write alphabet) then (
+    print_endline "write element must be part of alphabet field";
+    exit 1
+  );
+  let write_char = read.[0] in
+  let action = parse_action (trans_json |> Yojson.Basic.Util.member "action" |> Yojson.Basic.Util.to_string) in
+  {
+    read     = read_char;
+    to_state = to_state;
+    write    = write_char;
+    action   = action;
+  }
+
+let check_transitions_tab json =
+  let states = json |> Yojson.Basic.Util.member "states" |> Yojson.Basic.Util.to_list |> List.map Yojson.Basic.Util.to_string in
+  let assoc  = json |> Yojson.Basic.Util.member "transitions" |> Yojson.Basic.Util.to_assoc in
+  List.map (fun (state, trans_list) ->
+    if not (List.mem state states) then (
+      Printf.printf "State '%s' in transitions is not part of states field\n" state;
+      exit 1
+    );
+    (state, trans_list |> Yojson.Basic.Util.to_list |> List.map (parse_transition json))
+  ) assoc
+
+let get_info json : Types.configuration =
   try {
     name = (json |> Yojson.Basic.Util.member "name" |> Yojson.Basic.Util.to_string);
     alphabet = (check_alphabet json);
     blank = (check_blank json);
     states = (json |> Yojson.Basic.Util.member "states" |> Yojson.Basic.Util.to_list |> List.map Yojson.Basic.Util.to_string);
     initial = (check_initial json);
+    finals = (check_finals json);
+    transitions = (check_transitions_tab json);
   }
   with Yojson.Basic.Util.Type_error _ -> (
     print_endline "Missing field";
     exit 1
   )
 
+let action_to_string a =
+  match a with
+  | Types.Left  -> "LEFT"
+  | Types.Right -> "RIGHT"
+
+let print_transition (t : Types.transition) =
+  Printf.printf "  read: %c, to_state: %s, write: %c, action: %s\n"
+    t.read
+    t.to_state
+    t.write
+    (action_to_string t.action)
+
+let print_transitions transitions =
+  List.iter (fun (state, trans_list) ->
+    Printf.printf "State: %s\n" state;
+    List.iter print_transition trans_list
+  ) transitions
+
 let parser =
   check_args;
   let filename = Sys.argv.(1) in
     let json = check_file filename in
-      let machine = get_info json in
-        print_endline machine.name;
-        List.iter print_endline machine.alphabet;
-        print_endline machine.blank;
-        List.iter print_endline machine.states;
-        print_endline machine.initial
+      let configuration = get_info json in
+        print_endline configuration.name;
+        List.iter print_endline configuration.alphabet;
+        print_endline configuration.blank;
+        List.iter print_endline configuration.states;
+        print_endline configuration.initial;
+        List.iter print_endline configuration.finals;
+        print_transitions configuration.transitions
--- srcs/types.ml
+++ srcs/types.ml
@@ -1,7 +1,34 @@
-type machine = {
+type action =
+    | Left
+    | Right
+
+type transition = {
+    read : char;
+    write : char;
+    action : action;
+    to_state : string;
+}
+
+type tape = {
+    left : char list;
+    current : char;
+    right : char list;
+}
+
+type configuration = {
   name : string;
   alphabet : string list;
   blank : string;
   states : string list;
   initial : string;
+  finals : string list;
+  transitions : (string * transition list) list;
+}
+
+
+type machine = {
+    config            : configuration;
+    tape                 : tape;
+    state                : string;
+    transitions    : transition list;
 }

1d247773265275ffa6609d86df44d2eb0a957bf9
bepoisso <github@bepoisso.com>
2026-06-01 18:08:24 +0200

    wip: try to create a palindrome checker (only ab alphabet work)

--- srcs/main.ml
+++ srcs/main.ml
@@ -3,27 +3,73 @@ open Types
 let () =
 	let config =
 		{
-			name = "unary_sub";
-			alphabet = [ "1"; "."; "-"; "=" ];
+			name = "palindrome";
+			alphabet = [ "a"; "b"; "."; "X"; "Y"; "n"; "y" ];
 			blank = '.';
-			states = [ "scanright"; "eraseone"; "subone"; "skip"; "HALT" ];
+			states = [ "scanright"; "find_right_a"; "find_right_b"; "check_a"; "check_b"; "return_left"; "reject"; "accept"; "HALT" ];
 			initial = "scanright";
 			finals = [ "HALT" ];
 		}
 	in
-	let tape = Tape.create_tape "111-11=" config.blank in
+	let tape = Tape.create_tape "abab" config.blank in
 	let transitions =
 		[
-			{ read = '.'; write = '.'; action = Right; from_state = "scanright"; to_state = "scanright" };
-			{ read = '1'; write = '1'; action = Right; from_state = "scanright"; to_state = "scanright" };
-			{ read = '-'; write = '-'; action = Right; from_state = "scanright"; to_state = "scanright" };
-			{ read = '='; write = '.'; action = Left; from_state = "scanright"; to_state = "eraseone" };
-			{ read = '1'; write = '='; action = Left; from_state = "eraseone"; to_state = "subone" };
-			(* { read = '-'; write = '.'; action = Left; from_state = "eraseone"; to_state = "HALT" }; *)
-			{ read = '1'; write = '1'; action = Left; from_state = "subone"; to_state = "subone" };
-			{ read = '-'; write = '-'; action = Left; from_state = "subone"; to_state = "skip" };
-			{ read = '.'; write = '.'; action = Left; from_state = "skip"; to_state = "skip" };
-			{ read = '1'; write = '.'; action = Right; from_state = "skip"; to_state = "scanright" };
+			(*? scanright *)
+			{ read = '.'; write = '.'; action = Right ; from_state = "scanright"; to_state = "scanright" };
+			{ read = 'X'; write = 'X'; action = Right ; from_state = "scanright"; to_state = "scanright" };
+			{ read = 'Y'; write = 'Y'; action = Right; from_state = "scanright"; to_state = "accept" };
+			{ read = 'a'; write = 'X'; action = Right ; from_state = "scanright"; to_state = "find_right_a" };
+			{ read = 'b'; write = 'X'; action = Right; from_state = "scanright"; to_state = "find_right_b" };
+
+			(*? find_right_a *)
+			{ read = '.'; write = '.'; action = Left ; from_state = "find_right_a"; to_state = "check_a" };
+			{ read = 'Y'; write = 'Y'; action = Left ; from_state = "find_right_a"; to_state = "check_a" };
+			{ read = 'a'; write = 'a'; action = Right ; from_state = "find_right_a"; to_state = "find_right_a" };
+			{ read = 'b'; write = 'b'; action = Right ; from_state = "find_right_a"; to_state = "find_right_a" };
+			{ read = 'X'; write = 'X'; action = Right ; from_state = "find_right_a"; to_state = "find_right_a" };
+
+			(*? find_right_b *)
+			{ read = '.'; write = '.'; action = Left ; from_state = "find_right_b"; to_state = "check_b" };
+			{ read = 'Y'; write = 'Y'; action = Left ; from_state = "find_right_b"; to_state = "check_b" };
+			{ read = 'a'; write = 'a'; action = Right ; from_state = "find_right_b"; to_state = "find_right_b" };
+			{ read = 'b'; write = 'b'; action = Right ; from_state = "find_right_b"; to_state = "find_right_b" };
+			{ read = 'X'; write = 'X'; action = Right ; from_state = "find_right_b"; to_state = "find_right_b" };
+
+			(*? check_a *)
+			{ read = 'a'; write = 'Y'; action = Left ; from_state = "check_a"; to_state = "return_left" };
+			{ read = 'Y'; write = 'Y'; action = Left ; from_state = "check_a"; to_state = "check_a" };
+			{ read = 'b'; write = 'b'; action = Right; from_state = "check_a"; to_state = "reject" };
+			{ read = 'X'; write = 'X'; action = Right; from_state = "check_a"; to_state = "reject" };
+
+
+			(*? check_b *)
+			{ read = 'b'; write = 'Y'; action = Left ; from_state = "check_b"; to_state = "return_left" };
+			{ read = 'Y'; write = 'Y'; action = Left ; from_state = "check_b"; to_state = "check_b" };
+			{ read = 'a'; write = 'a'; action = Right ; from_state = "check_b"; to_state = "reject" };
+			{ read = 'X'; write = 'X'; action = Right ; from_state = "check_b"; to_state = "reject" };
+
+			(*? return_left *)
+			{ read = '.'; write = '.'; action = Right; from_state = "return_left"; to_state = "scanright" };
+			{ read = 'X'; write = 'X'; action = Right ; from_state = "return_left"; to_state = "scanright" };
+			{ read = 'Y'; write = 'Y'; action = Left ; from_state = "return_left"; to_state = "return_left" };
+			{ read = 'a'; write = 'a'; action = Left ; from_state = "return_left"; to_state = "return_left" };
+			{ read = 'b'; write = 'b'; action = Left ; from_state = "return_left"; to_state = "return_left" };
+
+			(*? reject *)
+			{ read = '.'; write = 'n'; action = Right ; from_state = "reject"; to_state = "HALT" };
+			{ read = 'a'; write = 'a'; action = Right ; from_state = "reject"; to_state = "reject" };
+			{ read = 'b'; write = 'b'; action = Right ; from_state = "reject"; to_state = "reject" };
+			{ read = 'X'; write = 'X'; action = Right ; from_state = "reject"; to_state = "reject" };
+			{ read = 'Y'; write = 'Y'; action = Right ; from_state = "reject"; to_state = "reject" };
+
+			(*? accept *)
+			{ read = '.'; write = 'y'; action = Right ; from_state = "accept"; to_state = "HALT" };
+			{ read = 'a'; write = 'a'; action = Right ; from_state = "accept"; to_state = "accept" };
+			{ read = 'b'; write = 'b'; action = Right ; from_state = "accept"; to_state = "accept" };
+			{ read = 'X'; write = 'X'; action = Right ; from_state = "accept"; to_state = "accept" };
+			{ read = 'Y'; write = 'Y'; action = Right ; from_state = "accept"; to_state = "accept" };
+
+
 		]
 	in
 	Simulator.run config transitions tape
